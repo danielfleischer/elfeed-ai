10c8f39e3304eca2d6fabb3b06d252d0c0c0bdaa
Daniel Fleischer <danflscr@gmail.com>
2026-03-01 20:31:23 +0200

    Melpa mention, version bump

--- elfeed-ai.el
+++ elfeed-ai.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2026  Daniel Fleischer
 
 ;; Author: Daniel Fleischer <danflscr@gmail.com>
-;; Version: 0.1.0
+;; Version: 0.3.0
 ;; Package-Requires: ((emacs "28.1") (elfeed "3.4.1") (gptel "0.9.9"))
 ;; Keywords: comm, news, ai
 ;; URL: https://github.com/danielfleischer/elfeed-ai
